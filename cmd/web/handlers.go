4f0e303504be206e8329533f2f3d0c09cd2d75e9
Billy West <billywest.jm@gmail.com>
2020-10-24 18:02:05 -0400

    Scaling Data Validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
+	"letsgo.net/snippetbox/pkg/forms"
 	"letsgo.net/snippetbox/pkg/models"
 )
 
@@ -46,48 +45,59 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	// title := r.PostForm.Get("title")
+	// content := r.PostForm.Get("content")
+	// expires := r.PostForm.Get("expires")
 
-	errors := make(map[string]string)
+	// errors := make(map[string]string)
 
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field cannit be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field is too long (maximum is 100 characters)"
-	}
+	// if strings.TrimSpace(title) == "" {
+	// 	errors["title"] = "This field cannit be blank"
+	// } else if utf8.RuneCountInString(title) > 100 {
+	// 	errors["title"] = "This field is too long (maximum is 100 characters)"
+	// }
 
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field cannot be blank"
-	}
+	// if strings.TrimSpace(content) == "" {
+	// 	errors["content"] = "This field cannot be blank"
+	// }
 
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
+	// if strings.TrimSpace(expires) == "" {
+	// 	errors["expires"] = "This field cannot be blank"
+	// } else if expires != "365" && expires != "7" && expires != "1" {
+	// 	errors["expires"] = "This field is invalid"
+	// }
 
-	if len(errors) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: errors,
-			FormData:   r.PostForm,
-		})
+	// if len(errors) > 0 {
+	// 	app.render(w, r, "create.page.tmpl", &templateData{
+	// 		FormErrors: errors,
+	// 		FormData:   r.PostForm,
+	// 	})
+	// 	return
+	// }
+
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
+
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
