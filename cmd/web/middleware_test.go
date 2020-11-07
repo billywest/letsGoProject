ae095f72cb87d9b5eaaafabefc8969f1dae3343d
Billy West <billywest.jm@gmail.com>
2020-11-06 23:50:11 -0500

    testing

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSecureHeaders(t *testing.T) {
+
+	rr := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	secureHeaders(next).ServeHTTP(rr, r)
+
+	rs := rr.Result()
+
+	frameOptions := rs.Header.Get("X-Frame-Options")
+	if frameOptions != "deny" {
+		t.Errorf("want %q; got %q", "deny", frameOptions)
+	}
+
+	xssProtection := rs.Header.Get("X-XSS-Protection")
+	if xssProtection != "1; mode=block" {
+		t.Errorf("want %q; got %q", "1; mode=block", xssProtection)
+	}
+
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer rs.Body.Close()
+
+	body, err := ioutil.ReadAll(rs.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+// func newTestApplication(t *testing.T) *application {
+// 	return &application{
+// 		errorLog: log.New(ioutil.Discard, "", 0),
+// 		infoLog:  log.New(ioutil.Discard, "", 0),
+// 	}
+// }
+
+// func TestRequiredAuthentication(t *testing.T) {
+// 	app := newTestApplication(t)
+// 	user := "test"
+// 	email := "test@gmail.com"
+// 	password := "Test@123"
+
+// 	rr := httptest.NewRecorder()
+// 	err := app.users.Insert(user, email, password)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("OK"))
+// 	})
+
+// 	r, err := http.NewRequest(http.MethodGet, "/snippet/create", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	id, err := app.users.Authenticate(email, password)
+// 	app.session.Put(r, "authenticatedUserID", id)
+// 	app.authenticate(next)
+// 	app.requireAuthentication(next).ServeHTTP(rr, r)
+
+// 	rs := rr.Result()
+
+// 	cacheControl := rs.Header.Get("Cache-Control")
+// 	if cacheControl != "no-store" {
+// 		t.Errorf("want %q; got %q", "no-store", cacheControl)
+// 	}
+
+// 	if rs.StatusCode != http.StatusOK {
+// 		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+// 	}
+
+// 	defer rs.Body.Close()
+
+// 	body, err := ioutil.ReadAll(rs.Body)
+
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	if string(body) != "OK" {
+// 		t.Errorf("want body to equal %q", "OK")
+// 	}
+// }
